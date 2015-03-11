36e60f420d546e6d829cc2018e9a0f9aba90a590
Pedro Mancheno <pedromancheno@gmail.com>
2015-03-11 23:18:53 +0100

    Add constraints and invalidate intrinsic size

--- TextFieldConstraintsExperiment/ViewController.m
+++ TextFieldConstraintsExperiment/ViewController.m
@@ -8,20 +8,28 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *firstTextField;
+
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.firstTextField addTarget:self
+                  action:@selector(textFieldDidChange)
+        forControlEvents:UIControlEventEditingChanged];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)textFieldDidChange
+{
+    [self.firstTextField invalidateIntrinsicContentSize];
 }
+
+
 
 @end
